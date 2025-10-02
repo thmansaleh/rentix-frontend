81625d2dfbe04478b2728c7041e14b4edb799cb6
othman <thman.saleh@gmail.com>
2025-10-02 10:56:24 +0400

    refr3f

--- src/app/cases/[id]/edit/tasks/EditTaskModal.js
+++ src/app/cases/[id]/edit/tasks/EditTaskModal.js
@@ -15,7 +15,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Calendar } from "@/components/ui/calendar"
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { CalendarIcon, Plus, File as FileIcon, CircleX, User2, Loader2 } from "lucide-react"
+import { CalendarIcon, Plus, File as FileIcon, CircleX, User2, Loader2, Upload } from "lucide-react"
 import { cn } from "@/lib/utils"
 
 function EditTaskModal({ 
