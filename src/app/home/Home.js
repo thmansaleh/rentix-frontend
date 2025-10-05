1614729fb10c4a559b252d023a5a40071a9eb2e3
othman <thman.saleh@gmail.com>
2025-10-05 23:41:31 +0400

    rfe3f

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -5,7 +5,7 @@ import SessionWithNoDecision from "./SessionWithNoDecision"
 
 function Home() {
   return (
-    <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4  gap-2">
+    <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-2">
         <SessionWithNoDecision />
         <SessionsWithDecision />
         <CasePetitions />
