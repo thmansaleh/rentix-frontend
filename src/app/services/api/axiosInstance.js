1a1ee05fcfa675b989e180f8a9dfa265bd1c3ba6
othman <thman.saleh@gmail.com>
2025-10-16 17:55:52 +0400

    rfref

--- src/app/services/api/axiosInstance.js
+++ src/app/services/api/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:8080/api",
-  baseURL: "http://localhost:8080/api",
-  // baseURL: "https://law-backend-woad.vercel.app/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "https://law-backend-woad.vercel.app/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
